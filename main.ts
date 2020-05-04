bb2a34393a0665545fdc120cbcda062f2a23eaa5
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-04 23:59:49 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    //demo.coolStuff()
+    
     basic.showNumber(9)
 })
-/*
-namespace demo {
-    //% block
-    export function coolStuff() {
-        basic.showString("x")
-    }
-}
-*/
